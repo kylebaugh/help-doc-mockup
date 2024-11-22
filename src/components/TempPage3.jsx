94e6e23e450d1054d685c52f49422ff2f00f443d
kylebaugh <kylebaugh1@gmail.com>
2024-11-22 13:53:55 -0700

    adjusted temp pages

--- src/components/TempPage3.jsx
+++ src/components/TempPage3.jsx
@@ -2,45 +2,48 @@ import React from "react";
 
 const TempPage3 = () => {
   return (
-    <div className="p-4 font-sans leading-6">
-      <h1 className="text-2xl text-center font-bold">Example Page 3</h1>
-      <br />
 
-      <h1  className="text-[#d9534f]">Understanding Hazardous Materials</h1>
+    <div className="p-4">
+    <h1 className="text-2xl text-center font-bold">Example Page 1</h1>
 
-      <img
-        src="https://plus.unsplash.com/premium_photo-1682146224885-7051da250554?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Hazardous Materials Overview"
-        className="w-full rounded-lg my-4"
-      />
-      <p>
-        Hazardous materials, also known as HAZMAT, are substances that pose a
-        risk to health, safety, or property. These materials are often used in
-        industries such as manufacturing, construction, and transportation.
-      </p>
-      <h2>What Are the Common Types of Hazardous Materials?</h2>
-      <p>
-        Hazardous materials can be classified into several categories,
-        including:
-      </p>
-      <ul>
-        <li>
-          <strong>Flammable substances:</strong> Gasoline, propane, and ethanol.
-        </li>
-        <li>
-          <strong>Corrosive chemicals:</strong> Acids, bases, and cleaning
-          agents.
-        </li>
-        <li>
-          <strong>Toxic materials:</strong> Pesticides, asbestos, and lead
-          compounds.
-        </li>
-      </ul>
-      <p>
-        Proper handling and storage of these materials are crucial to preventing
-        accidents and protecting the environment.
-      </p>
-    </div>
+    <p className="my-2">
+      Bacon ipsum dolor amet pork loin tail minim, short ribs kielbasa kevin
+      pastrami velit culpa doner in venison. In tri-tip dolore tail, elit
+      ribeye fatback. Non bresaola ex, aute velit qui pork drumstick. Venison
+      swine meatball, dolore ex salami voluptate picanha pork belly landjaeger
+      fugiat reprehenderit do. T-bone cupim in, filet mignon consectetur
+      dolore lorem commodo quis nisi ham hock flank magna esse.
+    </p>
+
+    <h3>Example Video</h3>
+
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/u31qwQUeGuM?si=iJmqkPZI99f24u8N"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+    <p className="my-2">
+      Anim landjaeger capicola jowl enim, dolore consectetur laborum shankle
+      commodo. Turducken et aute sausage drumstick swine exercitation buffalo
+      dolore salami porchetta. Landjaeger meatloaf consectetur in, id sirloin
+      ut capicola turducken ham hock ribeye chuck exercitation culpa salami.
+      Pariatur corned beef beef sunt cow, ullamco kevin jerky laboris meatloaf
+      venison mollit et tail.
+    </p>
+    <p className="my-2">
+      Nulla beef bresaola duis ut, irure nisi short ribs magna ad pork chop
+      shoulder eu sunt ribeye. Frankfurter chislic sirloin ad, turkey sausage
+      nostrud rump pastrami est lorem sint minim meatball sunt. Velit deserunt
+      elit picanha venison aute filet mignon magna anim reprehenderit doner.
+      Landjaeger qui pariatur irure alcatra ribeye.
+    </p>
+  </div>
+
   );
 };
 
