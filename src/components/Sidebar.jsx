55a15d8561ef7229b6ca624c68bb2d2620ca7de2
Sean Fagan <faganseanp@gmail.com>
2024-11-22 11:46:31 -0700

    sidebar and page widths are dynamic for viewport size, and width can be adjusted by dragging side of sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,126 +1,9 @@
 import React, { useState } from "react";
 import { MdArrowDropDown } from "react-icons/md";
 import { IoMdArrowDropup } from "react-icons/io";
+import menuItems from "../assets/menuItems";
 
-const menuItems = [
-  {
-    label: "Welcome",
-    children: [
-      { label: "Using the Search Screen", children: [] },
-      { label: "Password security", children: [] },
-      { label: "Work Station Set Up", children: [] },
-    ],
-  },
-  {
-    label: "Administration",
-    children: [
-      {
-        label: "System", children: [
-          {
-            label: "Site Configuration", children: [
-              { label: "Site Configuration Screen Reference", children: [] },
-              { label: "Setting Site Configurations", children: [] },
-            ]
-          },
-          {
-            label: "Edits", children: [
-              { label: "Edits Screen Reference", children: [] },
-              { label: "Modifying Lists of Values", children: [] },
-            ]
-          },
-        ]
-      },
-      {
-        label: "General", children: [
-          {
-            label: "Facility", children: [
-              { label: "Facility Screen Reference", children: [] },
-              { label: "Creating Your Facility", children: [] },
-            ]
-          },
-          {
-            label: "Action/Meeting", children: [
-              { label: "Action/Meeting Screen Reference", children: [] },
-              { label: "Creating Actions", children: [] },
-            ]
-          },
-        ]
-      },
-      { label: "Material", children: [] },
-      { label: "Waste", children: [] },
-      { label: "Queries", children: [] },
-    ],
-  },
-  {
-    label: "Material",
-    children: [
-      {
-        label: "Catalog",
-        children: [
-          {
-            label: "Company", children: [
-              { label: "Company Screen Reference", children: [] },
-              { label: "Creating a company record", children: [] },
-            ]
-          },
-          {
-            label: "Product Groups", children: [
-              { label: "Product Groups Screen Reference", children: [] },
-              { label: "Creating Product Groups", children: [] },
-            ]
-          },
-        ],
-      },
-      {
-        label: "Requests",
-        children: [],
-      },
-    ],
-  },
-  {
-    label: "HMMS Waste",
-    children: [
-      { label: "Flow of Waste through HMMS", children: [] },
-      {
-        label: "Characterization",
-        children: [
-          {
-            label: "Waste Steams", children: [
-              { label: "Waste Steam Screen Reference", children: [] },
-              { label: "Creating Waste Steam", children: [] },
-            ]
-          },
-          {
-            label: "Waste Profiles", children: [
-              { label: "Waste Profiles Screen Reference", children: [] },
-              { label: "Creating Waste Profiles", children: [] },
-            ]
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: "HMMS Reporting and Analytics",
-    children: [
-      {
-        label: "Reports and Analytics", children: [
-          { label: "Reports and Analytics Screen Reference", children: [] },
-          { label: "Creating Reports and Analytics", children: [] },
-        ]
-      },
-      {
-        label: "Air", children: [
-          { label: "HAPS Report", children: [] },
-          { label: "VOC Report", children: [] },
-          { label: "Emissions", children: [] },
-        ]
-      },
-    ],
-  },
-];
-
-const Sidebar = () => {
+const Sidebar = ({ width }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [expandedItems, setExpandedItems] = useState({});
 
@@ -163,7 +46,7 @@ const Sidebar = () => {
 
               </span>
               {hasChildren && (
-                <span className="ml-2">{isExpanded ? <IoMdArrowDropup /> : <MdArrowDropDown /> }</span>
+                <span className="ml-2">{isExpanded ? <IoMdArrowDropup /> : <MdArrowDropDown />}</span>
               )}
             </button>
             {isExpanded && hasChildren && (
@@ -182,7 +65,7 @@ const Sidebar = () => {
       {/* Desktop Sidebar (Always Visible) */}
       <div
         className="hidden md:block fixed left-0 bg-gray-200 shadow-md overflow-auto"
-        style={{ width: "35%", top: "4rem", height: "calc(100% - 4rem)" }} // Starts below header, 35% width
+        style={{ width: `${width}px`, top: "4rem", height: "calc(100% - 4rem)" }} // Starts below header, 35% width
       >
         <nav className="p-4 overflow-x:scroll">{renderMenu(menuItems)}</nav>
       </div>
